6a91f6f392f2eee945406d087beedd57c11fd873
Anusha Kartala <anusha@Radhas-MacBook-Air.local>
2020-10-11 20:08:47 -0500

    Book App 2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(subject_params)
+    if @book.save
+      redirect_to(books_path)
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(subject_params)
+      redirect_to(book_path(@book))
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to(books_path)
+  end
+
+  private
+
+  def subject_params
+    params.require(:book).permit(:title, :author, :genre, :price, :published_date)
+  end
+
+end
